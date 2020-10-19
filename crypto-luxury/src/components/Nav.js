826b3d852efe8cc60cb97c17d27f067f592ae930
carlsachs <sachscarl@gmail.com>
2020-10-19 01:37:46 -0400

    added basic nav with standard colors, moving to carousel

---
+++ crypto-luxury/src/components/Nav.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Button, Navbar, Nav, Form, FormControl, NavDropdown, DropdownButton } from "react-bootstrap";
+import styled from "styled-components";
+
+import "./Nav.css";
+
+const Navigation = () => {
+
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+            <div style={{
+                marginRight: "3%"
+            }}>
+            <Navbar.Brand href="#home">Crypto Luxury</Navbar.Brand>
+            </div>
+            <Nav className="mr-auto">
+            <Nav.Link style={{color: "gold"}} href="/">Home</Nav.Link>
+            <Nav.Link style={{color: "gold"}} href="#pricing">Team</Nav.Link>
+            <Nav.Link style={{color: "gold"}} href="#pricing">Services</Nav.Link>
+            <div style={{
+                marginLeft: "15%"
+            }}>
+            <DropdownButton className="bgnav" variant="warning" title="Products" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.2">Luxury Watches</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Cards</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">All Products</NavDropdown.Item>
+            </DropdownButton>
+            </div>
+            </Nav>
+            <div style={{
+                marginLeft: "1%"
+            }}>
+            <Form inline>
+            <FormControl type="text" placeholder="Search Products" className="mr-sm-2" />
+            <Button variant="dark">Search</Button>
+            </Form>
+            </div>
+        </Navbar>
+      </div>
+    )
+}
+
+export default Navigation;
