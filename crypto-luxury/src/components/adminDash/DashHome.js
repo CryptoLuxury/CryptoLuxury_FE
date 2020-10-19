9da707795b30803a7be5cabee517ad4c5217e760
carlsachs <sachscarl@gmail.com>
2020-10-19 05:09:52 -0400

    adding before I break everything

--- crypto-luxury/src/components/adminDash/DashHome.js
+++ crypto-luxury/src/components/adminDash/DashHome.js
@@ -2,8 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 //react-reveal
-import RubberBand from "react-reveal/RubberBand";
-import BelowStats from "./adminComponents/BelowStats";
+
 
 const DashHome = () => {
 
