926ada2a618bf9ad5cfd5bc4aaad30830c81c329
carlsachs <sachscarl@gmail.com>
2020-10-19 04:52:42 -0400

    removed my dash to implement creative Tim's, purchased with license specifically for this project

--- crypto-luxury/src/components/adminDash/DashHome.js
+++ crypto-luxury/src/components/adminDash/DashHome.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import marble from "./marble.jpg";
-
 //react-reveal
 import RubberBand from "react-reveal/RubberBand";
 import BelowStats from "./adminComponents/BelowStats";
@@ -10,67 +8,7 @@ import BelowStats from "./adminComponents/BelowStats";
 const DashHome = () => {
 
     return (
-
-        <div>
-
-        <div style={{
-            width: "100%",
-            background: "black",
-            height: "20vh",
-            display: "flex",
-            flexFlow: "row nowrap"
-        }}>
-
-            
-            <div style={{
-                background: "black",
-                width: "100%",
-                display: "flex",
-                flexFlow: "column nowrap",
-                textAlign: "center",
-                paddingTop: "1%" 
-            }}>
-                <h2 style={{color: "white"}}>Total Sales</h2>
-                <RubberBand>
-                <h1 style={{color: "lightgreen"}}>$74,658</h1>
-                </RubberBand>
-            </div>
-
-            <div style={{
-                background: "darkgrey",
-                width: "100%",
-                display: "flex",
-                flexFlow: "column nowrap",
-                textAlign: "center",
-                paddingTop: "1%" 
-            }}>
-                <h2 style={{color: "white"}}>Total Orders</h2>
-                <RubberBand delay={250}>
-                <h1 style={{color: "lightgreen"}}>1,245</h1>
-                </RubberBand>
-            </div>
-
-            <div style={{
-                background: "black",
-                width: "100%",
-                display: "flex",
-                flexFlow: "column nowrap",
-                textAlign: "center",
-                paddingTop: "1%" 
-            }}>
-                <h2 style={{color: "white"}}>Sales Today</h2>
-                <RubberBand delay={500}>
-                <h1 style={{color: "lightgreen"}}>$6,328</h1>
-                </RubberBand>
-            </div>
-
-        </div>
-
-            <div>
-                <BelowStats />
-            </div>
-
-    </div>
+        <div></div>
     )
 }
 
